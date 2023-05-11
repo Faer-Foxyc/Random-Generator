6605866abe68ec096d02988283a56615b65f6d14
Anton Fox <42889325+AntonFoxy@users.noreply.github.com>
2023-05-11 19:35:52 +0300

    Добавьте файлы проекта.

---
+++ Classes/NumberGenerator.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Генератор_случайных_чисел.Classes
+{
+    internal class NumberGenerator
+    {
+        int number { get; set; }
+
+        public NumberGenerator(int _number)
+        {
+            number = _number;
+        }
+
+        public int GenerateNumber()
+        {
+            int numb = 0;
+            try
+            {
+                Random random = new Random();
+                numb = random.Next(number);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка! Отрицательное число + {e.Message}");
+            }
+            return numb;
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using Генератор_случайных_чисел.Classes;
+
+//-------------------------Start program-----------------------------------------------------------------------------\\
+
+while (true)
+{
+    Console.Write("\nВведите число в диапазоне которого \nбудет сгенерировано случайное число(от 0 до ваше число): ");
+    string number = Console.ReadLine();
+    NumberGenerator numberGenerator = new NumberGenerator(Convert.ToInt32(number));
+    int genNumb = numberGenerator.GenerateNumber();
+    Console.WriteLine($"\nСгенерированное число: {genNumb}");
+
+    Console.Write("\nХотите продолжить? Да(0)/Нет(1): ");
+    string answer = Console.ReadLine();
+    if (Convert.ToInt32(answer) == 1)
+    {
+        break;
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------------------\\
